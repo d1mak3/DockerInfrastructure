d00c95d5a67505832d280bfbcd903a653c86208f
d1mak3 <d1mak3ger@gmail.com>
2022-05-29 08:17:36 +0300

    Created test

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,34 +1,52 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
 
 namespace Client
 {
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Content { get; set; }
+    }
+
     public static class Program
     {
-        private static async Task PostThread()
+        private static int countOfThreadsFinished = 0;       
+
+        static async Task ExecutePostRequest()
+        {      
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(new Message() { Id = 0, Content = "Hello world!", Sender = "Dima" });
+            var data = new System.Net.Http.StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = "http://localhost:8000/api/producer";
+            using var client = new HttpClient();
+
+            var response = await client.PostAsync(url, data);
+
+            string result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(countOfThreadsFinished);
+            countOfThreadsFinished += 1;
+        }
+
+        static async Task ExecuteGetRequest()
         {
-            Dictionary<string, string> values = new Dictionary<string, string>
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://localhost:8000/api/producer");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+            countOfThreadsFinished += 1;
+        }
+
+        public async static Task Main(string[] args)
+        {
+            for (int i = 0; i < 1000; ++i)
             {
-                
-            };
-
-            HttpContent content = new FormUrlEncodedContent(values);
-
-            HttpClient httpClient = new HttpClient();
-            var response = await httpClient.PostAsync("http://localhost:5000/api/producer?sender=dima&content=hello", content);
-            
+                await ExecuteGetRequest();
+                //await ExecutePostRequest();
+            }
         }
-
-        private static async Task ExecuteKafkaConsumer()
-        {            
-            HttpClient httpClient = new HttpClient();
-            var response = await httpClient.GetAsync("http://localhost:5000/api/consumer");
-
-        }
-
-        private async static Task Main()
-        {
-            await ExecuteKafkaConsumer();
-            //await PostThread();
-        }        
     }
 }
